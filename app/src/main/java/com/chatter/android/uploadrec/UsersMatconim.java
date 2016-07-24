97c5ed9e603aa3de1dbd098240b99ba6caa91178
liran <liranlevixx1@gmail.com>
2016-07-24 20:18:39 +0300

    matconim

--- app/src/main/java/com/chatter/android/uploadrec/UsersMatconim.java
+++ app/src/main/java/com/chatter/android/uploadrec/UsersMatconim.java
@@ -3,6 +3,7 @@ package com.chatter.android.uploadrec;
 import android.content.Context;
 import android.widget.ListView;
 
+import com.chatter.android.uploadrec.utilClasses.Ingredients;
 import com.google.android.gms.internal.zzaev;
 import com.google.android.gms.internal.zzafd;
 import com.google.firebase.FirebaseApp;
@@ -13,8 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UsersMatconim {
-    String user, recName, timeTillDone, category, people, worth, lvl, hollyday, halfy, process;
-    List<Ingredients> ingList;
+    String user, recName, timeTillDone, category, people, worth, lvl, hollyday, halfy, process, hezka,recId;
+    List<Ingredients> ingList = new ArrayList<>();
     Context context;
     String userId;
     String userPass;
@@ -26,9 +27,10 @@ public class UsersMatconim {
         this.user = user;
     }
 
-    public UsersMatconim(Context context, String userPass, String uid, String user, String recName, String timeTillDone, String category, String people, String worth, String lvl, String hollyday, String halfy, String process) {
-        this.context = context;
-        ingList = new ArrayList<>();
+    public UsersMatconim(String recId,String recName, String timeTillDone, String category, String people, String hezka, String worth, String lvl, String hollyday, String halfy, List<Ingredients> ingList, String process) {
+        this.recId=recId;
+        this.hezka = hezka;
+        this.ingList = ingList;
         this.recName = recName;
         this.timeTillDone = timeTillDone;
         this.category = category;
@@ -53,7 +55,24 @@ public class UsersMatconim {
 
         //saving the user under the UUID
         myRef.child(userId).setValue(user);
+        myRef.child(userId).setValue(userPass);
     }
 
+    public void saveMatcon() {
+        UsersMatconim userSm = new UsersMatconim(recId,recName, timeTillDone, category, people, hezka, worth, lvl, hollyday, halfy, ingList, process);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myMref=database.getReference("Matcon");
+        myMref.child(recId).setValue(recName);
+        myMref.child(recId).setValue(timeTillDone);
+        myMref.child(recId).setValue(category);
+        myMref.child(recId).setValue(people);
+        myMref.child(recId).setValue(hezka);
+        myMref.child(recId).setValue(worth);
+        myMref.child(recId).setValue(lvl);
+        myMref.child(recId).setValue(hollyday);
+        myMref.child(recId).setValue(halfy);
+        myMref.child(recId).setValue(ingList);
+        myMref.child(recId).setValue(process);
+    }
 
 }
