13eafb994e80b83dbc36d790410306fb31f1c494
dorin <zrihend@gmail.com>
2016-08-02 20:51:06 +0300

    Merge branch 'master' of C:\Users\dorin\AndroidStudioProjects\UploadRec with conflicts.

--- app/src/main/java/com/chatter/android/uploadrec/Login.java
+++ app/src/main/java/com/chatter/android/uploadrec/Login.java
@@ -1,13 +1,13 @@
 package com.chatter.android.uploadrec;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,37 +15,107 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import java.util.Arrays;
 
 public class Login extends Activity
     {
         private TextView info,info1;
         private LoginButton loginButton;
         protected CallbackManager callbackManager;
+        Context context;
+
+        //facebook from the internet
+        private static final String TAG = "SignInActivity";
+        private static final int RC_SIGN_IN = 9001;
+
+        private GoogleApiClient mGoogleApiClient;
+        private TextView mStatusTextView;
+        private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        context = this;
+
 
 //facebook login
         FacebookSdk.sdkInitialize(getApplicationContext());
-        info = (TextView)findViewById(R.id.info_FB);
-        info1 = (TextView)findViewById(R.id.info_FB1);
+        info = (TextView) findViewById(R.id.info_FB);
+        info1 = (TextView) findViewById(R.id.info_FB1);
         setContentView(R.layout.activity_login);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         callbackManager = CallbackManager.Factory.create();
+
+
 //Google login code
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
+        mStatusTextView = (TextView) findViewById(R.id.status);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult)
+                    {
+                    Intent i = new Intent(Login.this ,MainActivity.class);
+                    startActivity(i);
+                        String myResult =loginResult.getAccessToken().getUserId().toString();
+                        Toast.makeText(Login.this, myResult, Toast.LENGTH_SHORT).show();
+                        //  info.setText(myResult);
+
+                        // Toast.makeText(getApplicationContext(), loginResult.getAccessToken().getUserId() , Toast.LENGTH_LONG).show();
+//                  info.setText(
+//                          "User ID: "   + loginResult.getAccessToken().getUserId().toString());
+//
+//                  info1.setText(
+//                               "Auth Token: " + loginResult.getAccessToken().getToken().toString());
+                    }
+
+                    @Override
+                    public void onCancel()
+                    {
+                        info.setText("Login attempt canceled.");
+                    }
+
+                    @Override
+                    public void onError(FacebookException e)
+                    {
+                        info.setText("Login attempt failed.");
+                    }
+                });
+
+            }
+        });
+
     }
 
-//Faccebook login code
+
+
+
+ public void singInGoogle(View v)
+ {
+
+ }
+
         public void LoginFB(View v)
         {
 
@@ -76,6 +146,7 @@ public class Login extends Activity
                 }
             });
         }
+
         protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             this.callbackManager.onActivityResult(requestCode, resultCode, data);
             Intent i = new Intent(Login.this,MainActivity.class);
@@ -84,5 +155,14 @@ public class Login extends Activity
 
 
         //google code
-        //hello
-}
+
+
+
+
+
+
+
+    }
+
+
+
