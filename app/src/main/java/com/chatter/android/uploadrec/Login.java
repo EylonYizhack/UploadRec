ba25fffd416356e5c48b5ef42f3675f02c2f4ed4
Eylon Yizhack <eblaze94@gmail.com>
2016-08-06 20:06:13 +0300

    5/8/16 with facebook userName

--- app/src/main/java/com/chatter/android/uploadrec/Login.java
+++ app/src/main/java/com/chatter/android/uploadrec/Login.java
@@ -1,18 +1,14 @@
 package com.chatter.android.uploadrec;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.chatter.android.uploadrec.HomePage;
-import com.chatter.android.uploadrec.R;
-import com.chatter.android.uploadrec.UsersMatconim;
-import com.chatter.android.uploadrec.utilClasses.User;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -20,14 +16,20 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
 import com.facebook.Profile;
+import com.facebook.login.LoginFragment;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class Login extends Activity {
     private LoginButton loginButton;
+
     protected CallbackManager callbackManager;
     Context context;
     @Override
@@ -38,6 +40,8 @@ public class Login extends Activity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
         callbackManager = CallbackManager.Factory.create();
         updateWithToken(AccessToken.getCurrentAccessToken());
 
@@ -64,46 +68,57 @@ public class Login extends Activity {
             @Override
             public void onSuccess(final LoginResult loginResult)
             {
-             /*
-                new GraphRequest(
-                        AccessToken.getCurrentAccessToken(),
-                        "/{user-id}/permissions",
-                        null,
-                        HttpMethod.GET,
-                        new GraphRequest.Callback() {
-                            public void onCompleted(GraphResponse response) {
-            // handle the result
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            public String Name;
+
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                // Application code
+                                try {
+                                     Name = object.getString("name");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                Toast.makeText(Login.this, "Name : " + Name, Toast.LENGTH_LONG).show();
+                                //___________________________________
+                                new AsyncTask<Void,Void,Void>()
+                                {
+
+                                    @Override
+                                    protected void onPreExecute() {
+                                        super.onPreExecute();
+
+                                    }
+                                    @Override
+                                    protected void onPostExecute(Void aVoid) {
+                                        super.onPostExecute(aVoid);
+                                        Intent i = new Intent(Login.this, HomePage.class);
+                                        startActivity(i);
+                                    }
+                                    @Override
+                                    protected Void doInBackground(Void... params)
+                                    {
+
+                                        UsersMatconim umUser = new UsersMatconim(loginResult.getAccessToken().getUserId(),Name,"userImg",0);
+                                        umUser.saveUser();
+
+
+                                        return null;
+                                    }
+
+                                }.execute();
+                                //___________________________________
                             }
-                        }
-                ).executeAsync(); */
-
-
-                new AsyncTask<Void,Void,Void>()
-                {
-
-                    @Override
-                    protected void onPreExecute() {
-                        super.onPreExecute();
-
-                    }
-                    @Override
-                    protected void onPostExecute(Void aVoid) {
-                        super.onPostExecute(aVoid);
-                        Intent i = new Intent(Login.this, HomePage.class);
-                        startActivity(i);
-                    }
-                    @Override
-                    protected Void doInBackground(Void... params)
-                    {
-
-                            UsersMatconim umUser = new UsersMatconim(loginResult.getAccessToken().getUserId(),"need th user name","userImg",0);
-                            umUser.saveUser();
-
-
-                        return null;
-                    }
-
-                }.execute();
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
 
 
             }
@@ -112,6 +127,7 @@ public class Login extends Activity {
             public void onCancel()
             {
                 Toast.makeText(context,"canceled",Toast.LENGTH_SHORT).show();
+                LoginManager.getInstance().logOut();
             }
 
             @Override
@@ -123,8 +139,8 @@ public class Login extends Activity {
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         this.callbackManager.onActivityResult(requestCode, resultCode, data);
-
     }
+
 
     public void continueAsGuest(View view)
     {
