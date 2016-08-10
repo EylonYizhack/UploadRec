7570f731e6ff6d678311fe043bfc52106c0ec8b1
liran <liranlevixx1@gmail.com>
2016-08-10 21:13:06 +0300

    ADAPTER

--- app/src/main/java/com/chatter/android/uploadrec/fragmentsOfHomePage/BestTen.java
+++ app/src/main/java/com/chatter/android/uploadrec/fragmentsOfHomePage/BestTen.java
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 package com.chatter.android.uploadrec.fragmentsOfHomePage;
 
 
@@ -9,16 +15,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.chatter.android.uploadrec.BtenAdapter;
+import com.chatter.android.uploadrec.MainActivity;
 import com.chatter.android.uploadrec.R;
+import com.chatter.android.uploadrec.UsersMatconim;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BestTen extends Fragment {
     Context context;
+    ListView myList;
+   // List<UsersMatconim> matconList;
+
 
     public BestTen() {
         // Required empty public constructor
@@ -28,7 +45,12 @@ public class BestTen extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_best_ten,container,false);
+        context=getActivity();
+        View viewList = inflater.inflate(R.layout.fragment_best_ten, container, false);
+        myList=(ListView)viewList.findViewById(R.id.myList);
+        BtenAdapter ad =new BtenAdapter(context);
+        myList.setAdapter(ad);
+        return viewList;
 
     }
 
