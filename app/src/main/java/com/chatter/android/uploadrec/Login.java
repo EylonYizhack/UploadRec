73a4ec1d10a6957c6f3e6cfb20311fff0c547f07
Eylon Yizhack <eblaze94@gmail.com>
2016-10-26 19:08:23 +0300

    newnew

--- app/src/main/java/com/chatter/android/uploadrec/Login.java
+++ app/src/main/java/com/chatter/android/uploadrec/Login.java
@@ -13,6 +13,7 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -26,7 +27,7 @@ public class Login extends Activity {
         this.context=context;
 
         FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+       // AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
         loginButton = (LoginButton)findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
